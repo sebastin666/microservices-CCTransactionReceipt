0eee2bc3c9e4939168d9044b79b87fc4506d284d
SEBUJAS <SEBUJAS@SEBU>
2019-08-02 13:04:00 +0530

    1

---
+++ CCTransactionReceipt/src/main/java/com/itc/trn/mis/CcTransactionReceiptApplication.java
@@ -0,0 +1,13 @@
+package com.itc.trn.mis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CcTransactionReceiptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CcTransactionReceiptApplication.class, args);
+	}
+
+}
